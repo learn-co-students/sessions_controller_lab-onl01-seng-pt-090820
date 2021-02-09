09a1e1d141ee596cbc368d88601f67f667cf7a30
DavidRMorphew <morphewd@umich.edu>
2021-02-08 19:34:54 -0500

    Done.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,10 @@
 module SessionsHelper
+    def logged_in?
+        if session[:name].blank?
+            "Hello there"
+            link_to "Login", "/login"
+        else
+            tag(:h1, "Hi, #{session[:name]}.")
+        end
+    end
 end
